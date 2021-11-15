becbb62f4433083185cc2e1be92dc5a336467968
Chemoautotroph <niekaiyu990206@icloud.com>
2021-11-15 15:32:39 -0500

    add protocol

---
+++ config/mysqlConn.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var _db *gorm.DB
+
+func MysqlConn() {
+	username := "admin"
+	password := "admin"
+	host := "127.0.0.1"
+	port := 3306
+	Dbname := "pan"
+	timeout := "10s"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("连接数据库失败, error=" + err.Error())
+	}
+	sqlDB, _ := db.DB()
+
+	//设置数据库连接池参数
+	sqlDB.SetMaxOpenConns(100) //设置数据库连接池最大连接数
+	sqlDB.SetMaxIdleConns(20)  //连接池最大允许的空闲连接数，如果没有sql任务需要执行的连接数大于20，超过的连接会被连接池关闭。
+}
+
+//获取gorm db对象，其他包需要执行数据库查询的时候，只要通过tools.getDB()获取db对象即可。
+//不用担心协程并发使用同样的db对象会共用同一个连接，db对象在调用他的方法的时候会从数据库连接池中获取新的连接
+func GetDB() *gorm.DB {
+	return _db
+}
